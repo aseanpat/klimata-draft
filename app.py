099bed795a63b8cd244eb8bcd581dd702358ce68
Patrick Salamera <168300363+aseanpat@users.noreply.github.com>
2025-11-04 13:17:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ from streamlit_extras.metric_cards import style_metric_cards
 # ==========================
 st.set_page_config(
     page_title="KLIMATA Risk Dashboard",
-    page_icon="🌿"
+    page_icon="🌿",
+    layout="wide"
 )
 
 # ==========================
